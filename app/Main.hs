14113bf8266d2a49b81877cee9e6d0816a527bb6
Marcin Martowicz <marcinmartowicz9@gmail.com>
2025-02-14 11:58:15 +0100

    Command line arguments handling

--- app/Main.hs
+++ app/Main.hs
@@ -1,19 +1,73 @@
 module Main where
 
+import Data.Maybe (fromMaybe)
 import qualified Data.Text.Lazy as TL
 import qualified LLVM.Pretty
-import System.Environment
+import Options.Applicative
+import Prettyprinter
+import Prettyprinter.Render.Text
+import System.Directory (removeFile)
+import System.Process (callProcess)
 import qualified Yafl as Yafl
+
+data Options = Options
+  { srcPath :: String,
+    dstPath :: Maybe String,
+    dumpCore :: Bool,
+    emitLLVM :: Bool
+  }
+
+optionsParser :: Parser Options
+optionsParser =
+  Options
+    <$> argument
+      str
+      ( metavar "SRC"
+          <> help "Path to the source file"
+      )
+    <*> optional
+      ( strOption
+          ( short 'o'
+              <> metavar "OUTPUT"
+              <> help "Path to the output file (default: a.out or a.ll)"
+          )
+      )
+    <*> switch
+      ( long "dump-core"
+          <> help "Dump core intermediate representation"
+      )
+    <*> switch
+      ( long "emit-llvm"
+          <> help "Emit LLVM IR instead of executable"
+      )
+
+optionsInfo :: ParserInfo Options
+optionsInfo =
+  info
+    (optionsParser <**> helper)
+    ( fullDesc
+        <> progDesc "Compile a source file"
+        <> header "yafl-compiler - A simple functional language compiler"
+    )
 
 main :: IO ()
 main = do
-  args <- getArgs
-  let filePath = head args
-  source <- readFile filePath
-  -- let result = Yafl.pipeline source
-  -- print result
-  Yafl.debugPipeline source
-
-  let llvmMod = Yafl.pipelineLLVM source
-  let llvmSource = TL.unpack $ LLVM.Pretty.ppllvm llvmMod
-  writeFile "output.ll" llvmSource
+  opts <- execParser optionsInfo
+  programSource <- readFile (srcPath opts)
+  if dumpCore opts
+    then do
+      let core = Yafl.corePipeline programSource
+      putDoc (pretty core)
+      putStrLn ""
+    else do
+      let llvmMod = Yafl.llvmPipeline programSource
+          llvmSource = TL.unpack $ LLVM.Pretty.ppllvm llvmMod
+          defaultOutput = if emitLLVM opts then "a.ll" else "a.out"
+          outputPath = fromMaybe defaultOutput $ dstPath opts
+      if emitLLVM opts
+        then writeFile outputPath llvmSource
+        else do
+          let tmpFile = "tmp.ll"
+          writeFile tmpFile llvmSource
+          callProcess "clang-14" [tmpFile, "runtime.c", "-o", outputPath]
+          removeFile tmpFile
--- src/Yafl.hs
+++ src/Yafl.hs
@@ -1,61 +1,28 @@
-module Yafl where
+module Yafl
+  ( corePipeline,
+    llvmPipeline,
+  )
+where
 
-import ANF.Syntax as ANF
 import qualified ANF.ToANF
 import qualified CodeGen.LLVM
 import qualified Core.ClosureConv
 import qualified Core.Pretty
-import Core.Syntax as Core
+import qualified Core.Syntax
 import qualified Desugared.Desugar
 import qualified LLVM.AST
-import Prettyprinter
-import Prettyprinter.Render.Text
 import qualified Surface.Lexer
 import qualified Surface.Parser
 import qualified Typed.Typecheck
 
-pipeline :: String -> Core.Program
-pipeline source =
-  let tokens = Surface.Lexer.alexScanTokens source
-      program = Surface.Parser.parseProgram tokens
-      desugared = Desugared.Desugar.desugar program
-      typed = Typed.Typecheck.typecheck desugared
-      anf = ANF.ToANF.toANF typed
-      core = Core.ClosureConv.closureConv anf
-   in core
+corePipeline :: String -> Core.Syntax.Program
+corePipeline =
+  Core.ClosureConv.closureConv
+    . ANF.ToANF.toANF
+    . Typed.Typecheck.typecheck
+    . Desugared.Desugar.desugar
+    . Surface.Parser.parseProgram
+    . Surface.Lexer.alexScanTokens
 
-pipelineLLVM :: String -> LLVM.AST.Module
-pipelineLLVM source =
-  let tokens = Surface.Lexer.alexScanTokens source
-      program = Surface.Parser.parseProgram tokens
-      desugared = Desugared.Desugar.desugar program
-      typed = Typed.Typecheck.typecheck desugared
-      anf = ANF.ToANF.toANF typed
-      core = Core.ClosureConv.closureConv anf
-      llvm = CodeGen.LLVM.codeGen core
-   in llvm
-
---
-debugPipeline :: String -> IO ()
-debugPipeline source = do
-  let tokens = Surface.Lexer.alexScanTokens source
-  -- print tokens
-  -- print "Lexing done..."
-  let program = Surface.Parser.parseProgram tokens
-  -- print program
-  -- print "Parsing done..."
-  let desugared = Desugared.Desugar.desugar program
-  -- print desugared
-  -- print "Desugar done..."
-  let typed = Typed.Typecheck.typecheck desugared
-  -- print typed
-  -- print "Typecheck done..."
-  let anf = ANF.ToANF.toANF typed
-  -- print anf
-  -- print "ANF done..."
-  let core = Core.ClosureConv.closureConv anf
-  -- print core
-  -- print "Closure Conversion done..."
-  -- print "------------------"
-  putDoc (pretty core)
-  putStrLn ""
+llvmPipeline :: String -> LLVM.AST.Module
+llvmPipeline = CodeGen.LLVM.codeGen . corePipeline
